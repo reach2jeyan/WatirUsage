44555150b4ea1dfa0e1be3d0230a858a6ba11110
Mrityunjeyan <iamsmj@gmail.com>
2017-02-22 19:09:53 +0530

    Added comments

--- scheduler.rb
+++ scheduler.rb
@@ -10,12 +10,12 @@ ENV['TZ'] = 'Asia/Kolkata'
 scheduler = Rufus::Scheduler.new
 
 
-scheduler.every '15m' do
+scheduler.every '15m' do   #Since the server need to be run every time, to fetch the new update. Hence, a cron job. Need to find a code that would kill the job before activating another
     system("ruby bot.rb")
     puts "Server On"
 end
 
-scheduler.every '20m' do
+scheduler.every '20m' do  #Cron job that would fetch me the results, So what ever you code with watir and place it, you schedule that file to run here.
     puts "Cron job update in process. Please wait..."
     puts time
     system("ruby linkedindatascrap.rb")
