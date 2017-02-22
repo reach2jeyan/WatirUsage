8c2a960d90c0b26d1b5b4c1f6f4fa9d345a1c80a
Mrityunjeyan <iamsmj@gmail.com>
2017-02-22 20:08:59 +0530

    Changed the way message displayed

--- newsbot.rb
+++ newsbot.rb
@@ -1,8 +1,8 @@
 require 'watir'
-
+time = Time.now.strftime("%d/%m/%Y %H:%M")
 browser = Watir::Browser.new :chrome
 
 browser.goto 'http://thehindu.com'
 sleep(10)
 Breakingnews = browser.div(:class => 'col-xs-12 col-sm-6 col-md-6 col-lg-6 hover-icon').text
-File.open("Breakingnews.txt", "w") { |file| file.write("Hey Emjey! I have a breaking news update for you: #{Breakingnews}") }
+File.open("Breakingnews.txt", "w") { |file| file.write( "#{Breakingnews}" "\n" "\n" "The time I updated this is #{time}") }
